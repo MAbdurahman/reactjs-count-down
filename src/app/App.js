58643d68bc8ee9ad08f8c93e93bc737b5a8e9172
Mahdi Abdurrahman <mdbdrrhm@gmail.com>
2025-09-09 07:48:04 -0500

    Streamline project - remove unnecessary files

---
+++ src/app/App.js
@@ -0,0 +1,10 @@
+import React, {Fragment} from 'react';
+import CountDownTimerComponent from '../components/CountDownTimerComponent';
+
+export default function App() {
+	return (
+		<Fragment>
+			<CountDownTimerComponent />
+		</Fragment>
+	);
+}
