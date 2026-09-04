a7b388505bdbeec40ff5dc3da6eae1d492d66aa0
Mahdi Abdurrahman <mdbdrrhm@gmail.com>
2026-09-04 16:27:29 -0500

    Improve functionality of Preloader fading out

--- src/app/App.js
+++ src/app/App.js
@@ -1,22 +1,26 @@
-import React, {Fragment, useState, useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
 import CountDownTimerComponent from '../components/CountDownTimerComponent';
 import Preloader from '../components/Preloader';
 
 export default function App() {
-	const [isLoading, setIsLoading] = useState(true);
+   const [isLoading, setIsLoading] = useState(true);
 
-	useEffect(() => {
-		const timerId = setTimeout(() => {
-			setIsLoading(false);
-		}, 5000);
-		return () => clearTimeout(timerId);
+   useEffect(() => {
+      const timerId = setTimeout(() => {
+         setIsLoading(false);
+      }, 5000);
+      return () => clearTimeout(timerId);
 
-	}, []);
+   }, []);
 
-	return (
-		<Fragment>
-			{isLoading && <Preloader />}
-			<CountDownTimerComponent />
-		</Fragment>
-	);
+   return (
+      <div>
+         {isLoading ? (<Preloader/>)
+            :
+            <div className='fade-in'>
+               <CountDownTimerComponent/>
+            </div>
+         }
+      </div>
+   );
 }
