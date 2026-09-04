d66f372fc24673eac4aa99687449912089daf87e
Mahdi Abdurrahman <mdbdrrhm@gmail.com>
2026-09-04 05:32:17 -0500

    Add styled-components package and Preloader

--- src/app/App.js
+++ src/app/App.js
@@ -1,9 +1,21 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, useState, useEffect} from 'react';
 import CountDownTimerComponent from '../components/CountDownTimerComponent';
+import Preloader from '../components/Preloader';
 
 export default function App() {
+	const [isLoading, setIsLoading] = useState(true);
+
+	useEffect(() => {
+		const timerId = setTimeout(() => {
+			setIsLoading(false);
+		}, 5000);
+		return () => clearTimeout(timerId);
+
+	}, []);
+
 	return (
 		<Fragment>
+			{isLoading && <Preloader />}
 			<CountDownTimerComponent />
 		</Fragment>
 	);
